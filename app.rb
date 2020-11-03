ee9f101c9ef2caafd63247af87c91536599b12de
qytiz <qytizz@gmail.com>
2020-11-03 22:50:00 +0300

    Исправления
    Теперь станции могут отправлять поезда, а другие станции могут их получать, ваааааааау

--- app.rb
+++ app.rb
@@ -4,114 +4,120 @@ class Station
     @name=(name)
     @trains_list=[]
   end
+
   def add_train(train)
     @trains_list<<train
   end
-  def show_trains
-    @trains_list.each{ |train| puts train.number}
-  end
+
   def show_trains_by_type(type)
-    trainType=@trains_list.find_all{ |train| train.type == type}
-    puts "Количество поездов #{type} равно #{trainType.length}"
-    trainType.each{ |train| puts train}
+    puts @trains_list.find_all{ |train| train.type == type}
   end
+
   def send_train(train)
-    @trains_list.delete(train);
-    
+    if @trains_list.include? train
+    @trains_list.delete(train)
+    train.move_forward
+    end
   end
 end
 
 class Route
-  attr_reader :first,:optional_s,:last
+  attr_reader :first,:extra_stations,:last
   def initialize(first,last)
-   @first=first;
-   @last=last;
-   @optional_s=[];
+   @first=first
+   @last=last
+   @extra_stations=[]
   end
+
   def add_station(station)
-    @optional_s<<station;
+    @extra_stations<<station
   end
+
   def remove_station(station)
-    @optional_s.delete(station)
+    @extra_stations.delete(station)
   end
+
   def show_stations
-    puts @first.name;
-    @optional_s.each{|station|puts station.name};
-    puts @last.name;
+    puts @first.name
+    @extra_stations.each{|station|puts station.name}
+    puts @last.name
   end
 end
 
 class Train 
-  attr_reader :number,:type,:route;
+  attr_reader :number,:type,:route_serialized;
   def initialize(number,type,wagon_counter)
-    @number=number;
-    @type=type;
-    @wagon_counter=wagon_counter;
-    @speed=0;
-    @station_number=0;
-    @route=[]
+    @number=number
+    @type=type
+    @wagon_counter=wagon_counter
+    @speed=0
+    @station_number=0
+    @route_serialized=[]
   end
+
   def add_speed(speed)
-    @speed+=speed;
+    @speed+=speed
   end
-  def current_speed
-    print "Текущая скорость =";
-    puts @speed;
-  end
+
   def drop_speed
-    @speed=0;
+    @speed=0
   end
-  def current_wagon
-    print "Текущее число вагонов =";
-    puts @wagon_counter;
+
+  def add_wagon
+    @wagon_counter+=1
   end
-  def wagon(operation)
-    if !(@speed > 0)
-      if operation=='raise'
-        @wagon_counter+=1;
-      else
-        @wagon_counter-=1;
-      end
-    end
+  
+  def remove_wagon
+    @wagon_counter-=1
   end
+ 
   def route(route)
-  @route<<route.first;
-  @route+=route.optional_s;
-  @route<<route.last;
-  @current_station=@route[0];
+    @route_serialized<<route.first
+    if route.extra_stations.is_a?(Array)
+      @route_serialized+=route.extra_stations
+    end
+    @route_serialized<<route.last
+    @current_station=@route_serialized[0]
+    @current_station.add_train(self)
   end
+
   def move_forward
-  @station_number+=1;  
-  if @station_number<@route.length
-    @current_station=@route[@station_number];
-  else
-    @station_number-=1;
-  end
+    if get_next_station.is_a?(Station)
+      @current_station=get_next_station
+      @station_number+=1
+      @current_station.add_train(self)
+    end
   end
 
   def move_backward
-    @station_number-=1;  
-    if @station_number>=0
-      @current_station=@route[@station_number];
-    else
-      @station_number-=1;
+    if get_ex_station.is_a?(Station)
+      @current_station=get_ex_station
+      @station_number-=1
+      @current_station.add_train(self)
     end
   end
 
-  def show_route
-    if @station_number== @route.length-1
-      puts @route[@station_number-1].name;
-      puts "#{@route[@station_number].name}  <--";
-    else
-    if @station_number == 0
-      puts "#{@route[@station_number].name}  <--";
-      puts @route[1].name;
-    else
-      puts @route[@station_number-1].name;
-      puts "#{@route[@station_number].name}  <--";
-      puts @route[@station_number+1].name;
-    end
+  def get_ex_station
+  if @station_number>0
+    return @route_serialized[@station_number-1]
+  end
+  end
+
+  def get_current_station
+    return @route_serialized[@station_number]
+  end
+
+  def get_next_station
+    if @station_number<@route_serialized.length-1
+      return @route_serialized[@station_number+1]
     end
   end
+  
+  def show_route
+  puts get_ex_station
+  puts "#{get_current_station} <--"
+  puts get_next_station
+  end
+
 end
 
