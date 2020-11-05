fb503f81bcadad9c6859bafb5c08bfb3523f2154
qytiz <qytizz@gmail.com>
2020-11-05 13:04:00 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 class Station
   attr_reader :name, :trains_list
+
   def initialize(name)
-    @name = (name)
+    @name = name
     @trains_list = []
   end
 
@@ -9,25 +12,22 @@ class Station
     @trains_list << train
   end
 
-  def remove_train(train)
+  def send_train(train)
     @trains_list.delete(train)
   end
 
   def show_trains_by_type(type)
-    puts @trains_list.find_all{ |train| train.type == type}
-  end
-
-  def send_train(train)
-    remove_train(train)
+    puts @trains_list.find_all { |train| train.type == type }
   end
 end
 
 class Route
   attr_reader :first, :extra_stations, :last
+
   def initialize(first, last)
-   @first = first
-   @last = last
-   @extra_stations = []
+    @first = first
+    @last = last
+    @extra_stations = []
   end
 
   def add_station(station)
@@ -40,16 +40,18 @@ class Route
 
   def show_stations
     puts @first.name
-    @extra_stations.each{ |station| puts station.name }
+    @extra_stations.each { |station| puts station.name }
     puts @last.name
   end
+
   def stations
     [@first, *@extra_stations, @last]
   end
 end
 
-class Train 
-  attr_reader :number, :type, :route
+class Train
+  attr_reader :number, :type, :route, :current_station
+  
   def initialize(number, type, wagon_counter)
     @number = number
     @type = type
@@ -68,50 +70,42 @@ class Train
   def add_wagon
     @wagon_counter += 1
   end
-  
+
   def remove_wagon
     @wagon_counter -= 1
   end
- 
-  def route(route)
+
+  def add_route(route)
     @route = route
     @current_station = @route.stations[0]
     @current_station.add_train(self)
   end
 
   def move_forward
-      @current_station.remove_train(self)
-      @current_station = get_next_station
-      @current_station.add_train(self)
+    @current_station.send_train(self)
+    @current_station = next_station
+    @current_station.add_train(self)
   end
 
   def move_backward
-      @current_station.remove_train(self)
-      @current_station = get_ex_station
-      @current_station.add_train(self)
+    @current_station.send_train(self)
+    @current_station = previous_station
+    @current_station.add_train(self)
   end
 
-  def get_previous_station
-  if @route.stations.index(@current_station) > 0
-     @route.stations[@route.stations.index(@current_station) - 1]
-  end
+  def previous_station
+    @route.stations[@route.stations.index(@current_station) - 1] if @route.stations.index(@current_station).positive?
   end
 
-  def get_current_station
-     @current_station
-  end
-
-  def get_next_station
+  def next_station
     if @route.stations.index(@current_station) < @route.stations.length - 1
-       @route.stations[@route.stations.index(@current_station) + 1]
+      @route.stations[@route.stations.index(@current_station) + 1]
     end
   end
-  
+
   def show_location
-  puts get_previous_station
-  puts "#{get_current_station} <--"
-  puts get_next_station
+    puts previous_station
+    puts "#{@current_station} <--"
+    puts next_station
   end
-
 end
-
